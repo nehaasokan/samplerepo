b6b4c408c1a189b31128138277dcc915c2a69a19
Neha <neha.y.official@gmail.com>
2020-12-03 18:42:37 +1300

    Sample Task

---
+++ src/Programs/task1.java
@@ -0,0 +1,17 @@
+package Programs;
+
+public class task1 {
+
+	public static void main(String[] args) {
+		
+
+			
+          
+              
+               System.out.println("SAmple Program");
+        }
+
+}
+
+	
+
